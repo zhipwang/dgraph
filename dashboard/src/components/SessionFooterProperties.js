bd5e57ad4cb460b21b61173a9bdbe116f96656e1
Sung Won Cho <mikeswcho@gmail.com>
2017-05-17 14:00:13 +1000

    Display facets and only display expand if needed

--- dashboard/src/components/SessionFooterProperties.js
+++ dashboard/src/components/SessionFooterProperties.js
@@ -8,7 +8,8 @@ class SessionFooterProperties extends React.Component {
     super(props);
 
     this.state = {
-      isExpanded: false
+      isExpanded: false,
+      canExpand: false
     };
   }
 
@@ -20,24 +21,50 @@ class SessionFooterProperties extends React.Component {
     });
   };
 
+  // isEllipsisActive detects if .properties elements is overflown and hence
+  // ellipsis is active
+  isEllipsisActive = () => {
+    const propertiesEl = this._properties;
+    if (!propertiesEl) {
+      return false;
+    }
+
+    return propertiesEl.clientWidth < propertiesEl.scrollWidth;
+  };
+
+  componentDidMount() {
+    const canExpand = this.isEllipsisActive();
+
+    if (canExpand) {
+      this.setState({ canExpand: true });
+    }
+  }
+
   render() {
     const { entity } = this.props;
-    const { isExpanded } = this.state;
+    const { canExpand, isExpanded } = this.state;
 
     const nodeProperties = JSON.parse(entity.title);
 
     // Nodes have facets and attrs keys.
     const isNode = Object.keys(nodeProperties).length !== 1;
-    const attrs = nodeProperties.attrs || {};
+    const attrs = nodeProperties.attrs;
+    const facets = nodeProperties.facets;
+    const facetKeys = Object.keys(facets);
 
     return (
       <div className="properties-container">
-        <div className={classnames("properties", { expanded: isExpanded })}>
-          <span className="label label-default entity-type-label">
+        <div
+          className={classnames("properties", { expanded: isExpanded })}
+          ref={el => {
+            this._properties = el;
+          }}
+        >
+          <span className="label label-info">
             {isNode ? "Node" : "Edge"}
           </span>
 
-          {isNode
+          {isNode && attrs
             ? Object.keys(attrs).map(function(key, idx) {
                 return (
                   <span className="property-pair" key={idx}>
@@ -51,17 +78,39 @@ class SessionFooterProperties extends React.Component {
                 );
               })
             : null}
+
+          {isNode && facetKeys.length > 0
+            ? <span>
+                <span className="label label-default">
+                  Facets
+                </span>
+                {Object.keys(facets).map(function(key, idx) {
+                  return (
+                    <span className="property-pair" key={idx}>
+                      <span className="property-key">
+                        <span className="key-content">{key}</span>:
+                      </span>
+                      <span className="property-val">
+                        {String(facets[key])}
+                      </span>
+                    </span>
+                  );
+                })}
+              </span>
+            : null}
         </div>
 
-        <a
-          href="#toggle-expand"
-          onClick={this.handleToggleExpand}
-          className="toggle-expand-btn"
-        >
-          {isExpanded
-            ? <i className="fa fa-caret-up" />
-            : <i className="fa fa-caret-down" />}
-        </a>
+        {canExpand
+          ? <a
+              href="#toggle-expand"
+              onClick={this.handleToggleExpand}
+              className="toggle-expand-btn"
+            >
+              {isExpanded
+                ? <i className="fa fa-caret-up" />
+                : <i className="fa fa-caret-down" />}
+            </a>
+          : null}
       </div>
     );
   }
