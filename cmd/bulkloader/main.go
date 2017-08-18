9d16d6ce0b2501e7a0d05cf8767168ae96f96b8b
Peter Stace <peter@dgraph.io>
2017-08-18 18:55:37 +1000

    Start of bulk loader

---
+++ cmd/bulkloader/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"compress/gzip"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/dgraph-io/dgraph/rdf"
+	"github.com/dgraph-io/dgraph/x"
+)
+
+func main() {
+
+	rdfFile := flag.String("r", "", "Location of rdf file to load")
+	flag.Parse()
+
+	lastUID := 0
+	uids := make(map[string]uint64)
+
+	// Load RDF
+	f, err := os.Open(*rdfFile)
+	x.Check(err)
+	defer f.Close()
+	gr, err := gzip.NewReader(f)
+	x.Check(err)
+	sc := bufio.NewScanner(gr)
+	for sc.Scan() {
+		x.Check(sc.Err())
+
+		//fmt.Printf("%s\n", sc.Text())
+
+		nq, err := rdf.Parse(sc.Text())
+		x.Check(err)
+		fmt.Printf("%#v\n", nq)
+	}
+}
