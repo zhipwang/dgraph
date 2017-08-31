9d52c616218d617b8af59057ba3b94c3c0cb8969
Peter Stace <peter@dgraph.io>
2017-08-31 12:38:04 +1000

    Simplify batch writer

--- cmd/bulkloader/async_batch_writer.go
+++ cmd/bulkloader/async_batch_writer.go
@@ -1,62 +1,50 @@
 package main
 
 import (
-	"sync"
 	"sync/atomic"
 
 	"github.com/dgraph-io/badger"
 	"github.com/dgraph-io/dgraph/x"
 )
 
-// TODO: The approach here isn't the simplest. A simpler approach would be to
-// add to a list in `Set`, and then do the async call in `Set` itself. `Wait`
-// can set any leftovers, and then wait for all asyncs to complete.
-
 const batchSize = 1000 // TODO: Should be parameterised
 
 type KVWriter struct {
-	kv   *badger.KV
-	ch   chan *badger.Entry
-	wg   sync.WaitGroup
-	prog *progress
+	kv    *badger.KV
+	batch []*badger.Entry
+	prog  *progress
 }
 
 func NewKVWriter(kv *badger.KV, prog *progress) *KVWriter {
 	w := &KVWriter{
-		kv:   kv,
-		ch:   make(chan *badger.Entry),
-		prog: prog,
+		kv:    kv,
+		batch: make([]*badger.Entry, 0, batchSize),
+		prog:  prog,
 	}
-	go w.recvEntries()
 	return w
 }
 
 func (w *KVWriter) Set(k, v []byte, meta byte) {
-	w.wg.Add(1)
-	w.ch <- &badger.Entry{
+	e := &badger.Entry{
 		Key:      k,
 		Value:    v,
 		UserMeta: meta,
 	}
+	w.batch = append(w.batch, e)
+	if len(w.batch) == batchSize {
+		w.setEntries(w.batch)
+		w.batch = make([]*badger.Entry, 0, batchSize)
+	}
 }
 
 func (w *KVWriter) Wait() {
-	close(w.ch)
-	w.wg.Wait()
-}
-
-func (w *KVWriter) recvEntries() {
-	es := make([]*badger.Entry, 0, batchSize)
-	for e := range w.ch {
-		es = append(es, e)
-		if len(es) >= batchSize {
-			w.setEntries(es)
-			es = make([]*badger.Entry, 0, batchSize)
-		}
+	atomic.AddInt64(&w.prog.outstandingWrites, 1)
+	err := w.kv.BatchSet(w.batch)
+	x.Check(err)
+	for _, e := range w.batch {
+		x.Check(e.Error)
 	}
-	if len(es) > 0 {
-		w.setEntries(es)
-	}
+	atomic.AddInt64(&w.prog.outstandingWrites, -1)
 }
 
 func (w *KVWriter) setEntries(entries []*badger.Entry) {
@@ -66,7 +54,6 @@ func (w *KVWriter) setEntries(entries []*badger.Entry) {
 		for _, e := range entries {
 			x.Check(e.Error)
 		}
-		w.wg.Add(-len(entries))
 		atomic.AddInt64(&w.prog.outstandingWrites, -1)
 	})
 }
