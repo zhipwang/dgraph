7202cea197f002a98c04c426e4c3dc2bafb34932
Peter Stace <peter@dgraph.io>
2017-09-04 11:54:54 +1000

    New experiment: write to a heap of smaller sorted files

--- cmd/bulkloader/async_batch_writer.go
+++ cmd/bulkloader/async_batch_writer.go
@@ -1,96 +1,87 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
-	"math/rand"
 	"os"
-	"sync"
-	"sync/atomic"
+	"sort"
 
 	"github.com/dgraph-io/badger"
 	"github.com/dgraph-io/dgraph/x"
 )
 
 const (
-	// Seem to not matter much. Tried 100, 1000, and 10000 and got same result.
-	writeBatchSize = 1000
+// Seem to not matter much. Tried 100, 1000, and 10000 and got same result.
+//writeBatchSize = 1000
 )
 
-type KVWriter struct {
-	//kv    *badger.KV
-	batch []*badger.Entry
-	prog  *progress
-	fd    *os.File
-
-	batchCh chan []*badger.Entry
-	doneWg  sync.WaitGroup
+type entry struct {
+	k, v []byte
 }
+
+type KVWriter struct {
+	////kv    *badger.KV
+	//batch []*badger.Entry
+	//prog *progress
+	//fd    *os.File
+
+	//batchCh chan []*badger.Entry
+	//doneWg  sync.WaitGroup
+
+	prog  *progress
+	batch []entry
+	sz    int
+
+	fileCount int
+
+	filename string
+}
+
+const batchSize = 100 << 20
 
 func NewKVWriter(kv *badger.KV, prog *progress, filename string) *KVWriter {
 
-	fd, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
-	fmt.Printf("Writing to %s\n", filename)
-	x.Check(err)
-
 	w := &KVWriter{
-		//kv:    kv,
-		batch:   make([]*badger.Entry, 0, writeBatchSize),
-		prog:    prog,
-		fd:      fd,
-		batchCh: make(chan []*badger.Entry, 2000), // can do 2000 unfinished writes at a time just like badger
+		prog:     prog,
+		filename: filename,
 	}
-
-	go w.doWrites()
-
 	return w
 }
 
 func (w *KVWriter) Set(k, v []byte, meta byte) {
-	e := &badger.Entry{
-		Key:      k,
-		Value:    v,
-		UserMeta: meta,
-	}
-	w.batch = append(w.batch, e)
-	if len(w.batch) == writeBatchSize {
-		w.setEntries(w.batch)
-		w.batch = make([]*badger.Entry, 0, writeBatchSize)
+
+	w.batch = append(w.batch, entry{k, v})
+	w.sz += len(k) + len(v)
+
+	if w.sz > batchSize {
+		w.dumpFile()
 	}
 }
 
 func (w *KVWriter) Wait() {
-	w.setEntries(w.batch)
-	close(w.batchCh)
-	w.doneWg.Wait()
-	x.Check(w.fd.Close())
+
+	if w.sz > 0 {
+		w.dumpFile()
+	}
 }
 
-func (w *KVWriter) setEntries(entries []*badger.Entry) {
-	atomic.AddInt64(&w.prog.outstandingWrites, 1)
-	w.batchCh <- entries
-}
+func (w *KVWriter) dumpFile() {
 
-func (w *KVWriter) doWrites() {
-	threshold := int(rand.Float64()*10 + 10)
-	w.doneWg.Add(1)
-	var buf bytes.Buffer
-	for entries := range w.batchCh {
-		for _, e := range entries {
-			buf.Write(e.Key)
-			buf.Write(e.Value)
-			buf.Write([]byte{e.UserMeta})
-			buf.WriteString("\n")
-		}
-		if buf.Len() > threshold<<20 {
-			x.Check2(w.fd.Write(buf.Bytes()))
-			buf.Reset()
-		}
-		atomic.AddInt64(&w.prog.outstandingWrites, -1)
-	}
-	if buf.Len() > 0 {
-		x.Check2(w.fd.Write(buf.Bytes()))
+	sort.Slice(w.batch, func(i, j int) bool { return bytes.Compare(w.batch[i].k, w.batch[j].k) < 0 }) // TODO Slow?
+
+	w.fileCount++
+	fd, err := os.OpenFile(fmt.Sprintf("%s_%d", w.filename, w.fileCount), os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+	x.Check(err)
+	defer func() { x.Check(fd.Close()) }()
+
+	wr := bufio.NewWriterSize(fd, 32<<20)
+	for _, entry := range w.batch {
+		// TODO: would also need to write sizes of keys and values to read them later.
+		wr.Write(entry.k)
+		wr.Write(entry.v)
 	}
 
-	w.doneWg.Done()
+	w.sz = 0
+	x.Check(wr.Flush())
 }
