5a23942df9162661b857e0ebf27aa9db689b02c9
Peter Stace <peter@dgraph.io>
2017-09-04 13:45:23 +1000

    Work around bug in batch reset

--- cmd/bulkloader/async_batch_writer.go
+++ cmd/bulkloader/async_batch_writer.go
@@ -101,7 +101,7 @@ func (w *KVWriter) dumpFile() {
 		wr.Write(entry.v)
 		wr.WriteString("\n")
 	}
-	w.batch = w.batch[:0]
+	w.batch = nil // TODO: was taking w.batch[:0] before, but this was causing some problem
 	x.Check(wr.Flush())
 	atomic.AddInt64(&w.prog.outstandingWrites, -1)
 	x.Check(fd.Close())
